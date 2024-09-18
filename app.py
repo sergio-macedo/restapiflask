9f9f063c7c4a2d01501d81963e9a8b53166ea768
sergio-macedo <you@example.com>
2024-09-18 10:24:41 +0200

    fix: inverted the order of the starting of app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 from flask import Flask
+import json
 from flask_restful import Resource, Api
-
+from flask_mongoengine import MongoEngine
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'port': 27017,
+    'host': 'mongodb',
+    'username': 'admin',  
+    'password': 'admin'
+}
+
 api = Api(app)
+db = MongoEngine(app)
+
+
+
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.EmailField(required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
@@ -18,8 +40,6 @@ class User(Resource):
         return("message" "CPF")
 
 api.add_resource(Users, '/users')
-
-
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
