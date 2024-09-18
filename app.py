ecefc97174c44edc8697e41919d026b2d5fd0346
sergio-macedo <you@example.com>
2024-09-18 11:51:56 +0200

    feat: created the first try catch

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 import json
 from flask_restful import Resource, Api
 from flask_mongoengine import MongoEngine
+from MongoEngine import NotUniqueError
+import re
+
+
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
@@ -11,6 +15,33 @@ app.config['MONGODB_SETTINGS'] = {
     'username': 'admin',  
     'password': 'admin'
 }
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('first_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('last_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('cpf',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('email',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('birth_date',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
 
 api = Api(app)
 db = MongoEngine(app)
@@ -27,15 +58,48 @@ class UserModel(db.Document):
     birth_date = db.DateTimeField(required=True)
 
 
-class Users(Resource):
-    def get(self):
-        return {'message': 'user 1'}
-
-
 class User(Resource):
-    def post(self):
-        return {'message': 'teste'}
 
+    def validate_cpf(self, cpf):
+
+        # Has the correct mask?
+        if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+            return False
+
+        # Grab only numbers
+        numbers = [int(digit) for digit in cpf if digit.isdigit()]
+
+        # Does it have 11 digits?
+        if len(numbers) != 11 or len(set(numbers)) == 1:
+            return False
+
+        # Validate first digit after -
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:9],
+                                                  range(10, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[9] != expected_digit:
+            return False
+
+        # Validate second digit after -
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:10],
+                                                  range(11, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[10] != expected_digit:
+            return False
+
+        return True
+
+
+    def post(self):
+        data = _user_parser.parse_args()
+        if not self.validate_cpf(data["cpf"]):
+            return {"message": "CPF is invalid"}, 400
+
+        try:    
+            response = UserModel(**data).save()
+            return {"message":"user %s successfully created", %response.id } 
+        except NotUniqueError:
+            return {"message": "CPF already existis in database"} , 400
     def get(self, cpf):
         return("message" "CPF")
 
