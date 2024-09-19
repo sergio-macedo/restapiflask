1817cd94fc4dc984a79798c52b1404e9ca1d00c1
sergio-macedo <you@example.com>
2024-09-19 17:28:25 +0200

    feat: add delete endpoint

--- application/app.py
+++ application/app.py
@@ -97,3 +97,12 @@ class User(Resource):
             return {"message": "User updated"}, 200
         else:
             return {"message": "User does not exist in database!"}, 400
+
+    def delete(self, cpf):
+        response = UserModel.objects(cpf=cpf)
+
+        if response:
+            response.delete()
+            return {"message": "User deleted"}, 200
+        else:
+            return {"message": "User does not exist in database!"}, 400
